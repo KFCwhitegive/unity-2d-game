c63960ff8dc788b665a2493f5a36f76689f7e82d
zZoOAustinOoZz <austinzhao5404@gmail.com>
2024-09-24 14:19:01 +1200

    Adding player input system
    Adding player input system, and changes the scene setting. adding a new scene, adding player scripts.
    No coding added

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+}
